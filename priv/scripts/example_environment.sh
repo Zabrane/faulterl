8f155c7973d19ca9a183ce00f71b57957df36a14
Zabrane <zabrane3@gmail.com>
2025-04-18 02:00:54 +0200

    Update example_environment.sh

--- priv/scripts/example_environment.sh
+++ priv/scripts/example_environment.sh
@@ -5,7 +5,7 @@ DIR=$1
 case `uname -s` in
     Darwin)
         LIB="$DIR/intercept.stub.dylib"
-        echo DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_SHARED_REGION=avoid DYLD_INSERT_LIBRARIES=${LIB}:/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/ATS.framework/Versions/A/Resources/libFontRegistry.dylib
+        echo DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_SHARED_REGION=avoid DYLD_INSERT_LIBRARIES=${LIB}:/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/ATS.framework/Versions/A/Resources/libFontRegistryUI.dylib
     ;;
     Linux)
         LIB="$DIR/intercept.stub.so"
